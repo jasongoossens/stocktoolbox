9f8e25bb1139c8de13b54fb15beda95ebc1f88d2
Jason Goossens <jason.goossens@gmail.com>
2020-08-04 13:02:49 +0200

    first api call

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('config');
+const axios = require('axios');
 
 const server = express();
 
@@ -7,4 +8,16 @@ const port = 3000;
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
-console.log(config.get('apiKey'));
+// supported symbols call
+const baseUrl = 'https://finnhub.io/api/v1/';
+const endpoint = 'stock/symbol';
+
+try {
+  axios
+    .get(baseUrl + endpoint, {
+      params: { exchange: 'US', token: config.get('apiKey') },
+    })
+    .then((response) => console.log(response));
+} catch (error) {
+  console.log(error);
+}
