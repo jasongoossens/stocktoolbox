54c58a27a49cc2f32ff33e168042ee04a93b3431
Jason Goossens <jason.goossens@gmail.com>
2020-08-06 01:15:39 +0200

    init ejs views

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const server = express();
 const finnhubRouter = require('./routes/finnhub');
 
 // server configuration
+server.set('view engine', 'ejs');
 server.use(express.json());
 server.use('/api/stocks', finnhubRouter);
 const port = 3000;
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
+
+server.get('/', (req, res) => res.render('index', { title: 'Index' }));
+server.use((req, res) =>
+  res.status(404).render('404', { title: 'Page not found' })
+);
+
+// https://stackoverflow.com/questions/25225240/how-do-i-store-a-value-from-an-api-call-with-node-js-express-and-mikeals-npm-re
