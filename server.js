ec2ee5151d77a0ac9699126f94cae83ee2071d86
Jason Goossens <jason.goossens@gmail.com>
2020-08-06 16:06:43 +0200

    refactored server config

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const server = express();
-const main = require('./routes/main');
-const finnhubRouter = require('./routes/finnHub');
-
-// server configuration
-server.set('view engine', 'ejs');
 server.use(express.json());
-server.use('/api/stocks', finnhubRouter);
+const main = require('./routes/main');
 server.use('/', main);
+const finnhubRouter = require('./routes/finnHub');
+server.use('/api/stocks', finnhubRouter);
+
+server.set('view engine', 'ejs');
 const port = 3000;
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
