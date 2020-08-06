137bf5397b037d9da426ed48c5931a4671baf809
Jason Goossens <jason.goossens@gmail.com>
2020-08-06 16:06:23 +0200

    init search functionality

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const config = require('config');
+const c = require('config');
 
 const baseUrl = 'https://finnhub.io/api/v1/';
+const token = config.get('finnHubApiKey');
 
 router.get('/', (req, res) => {
   let data = [];
@@ -11,14 +13,14 @@ router.get('/', (req, res) => {
     axios.get(baseUrl + 'stock/profile2', {
       params: {
         symbol: 'tsla',
-        token: config.get('finnHubApiKey'),
+        token: token,
       },
     }),
     axios.get(baseUrl + 'stock/metric', {
       params: {
         symbol: 'tsla',
         metric: 'all',
-        token: config.get('finnHubApiKey'),
+        token: token,
       },
     }),
   ])
@@ -28,6 +30,40 @@ router.get('/', (req, res) => {
         data.push(r.data);
       });
       res.render('index', { title: 'Index', data });
+    })
+    .catch((error) => console.log('Something went wrong:', error));
+});
+
+router.get('/search', (req, res) => {
+  let data = [];
+  const ticker = req.query.ticker.toUpperCase();
+  Promise.all([
+    axios.get(baseUrl + 'stock/profile2', {
+      params: {
+        symbol: ticker,
+        token: token,
+      },
+    }),
+    axios.get(baseUrl + 'quote', {
+      params: {
+        symbol: ticker,
+        token: token,
+      },
+    }),
+  ])
+    .then((response) => {
+      response.map((r) => {
+        // data = r.data;
+        data.push(r.data);
+        console.log(data);
+      });
+      const [company, price] = data;
+      console.log(company);
+      res.render('search', {
+        title: ticker + ': ' + company.name,
+        company,
+        price,
+      });
     })
     .catch((error) => console.log('Something went wrong:', error));
 });
