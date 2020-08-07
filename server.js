59f991bea2f324911808a736f3f91f416ef08d13
Jason Goossens <jason.goossens@gmail.com>
2020-08-07 02:13:41 +0200

    added use of static files

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const server = express();
+server.set('view engine', 'ejs');
 server.use(express.json());
-const main = require('./routes/main');
-server.use('/', main);
+server.use(express.static('public'));
+server.use(
+  express.static('node_modules/@fortawesome/fontawesome-free/css/all.css')
+);
+const index = require('./routes/index');
+server.use(index);
+const stock = require('./routes/stock');
+server.use('/stock', stock);
 const finnhubRouter = require('./routes/finnHub');
 server.use('/api/stocks', finnhubRouter);
 
-server.set('view engine', 'ejs');
 const port = 3000;
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
