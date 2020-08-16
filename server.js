0cb12732f0f319859962559442961a8f4b8fe69a
Jason Goossens <jason.goossens@gmail.com>
2020-08-17 00:57:58 +0200

    changed port config

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.use(index);
 const stock = require('./routes/stock');
 server.use('/stock', stock);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
