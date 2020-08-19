6bec3269ee40f7dafa60c85990162c9cb9b04743
Jason Goossens <jason.goossens@gmail.com>
2020-08-20 00:43:27 +0200

    complete logic for theme switching

--- public/utils/helpers.js
+++ public/utils/helpers.js
@@ -1,3 +1,16 @@
 function switchTheme() {
-  console.log('Switched');
+  const toggler = document.querySelector('#toggler');
+  const body = document.body;
+
+  if (body.classList.contains('barebones')) {
+    toggler.classList.remove('fa-star-o');
+    body.classList.remove('barebones');
+    toggler.classList.add('fa-star');
+    body.classList.add('colorful');
+  } else {
+    body.classList.remove('colorful');
+    toggler.classList.remove('fa-star');
+    body.classList.add('barebones');
+    toggler.classList.add('fa-star-o');
+  }
 }
