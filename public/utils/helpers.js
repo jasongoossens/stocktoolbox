caf3c7dce42639c8ad40e9ae68eb808b4f6a5588
Jason Goossens <jason.goossens@gmail.com>
2020-08-20 10:39:54 +0200

    theme styling

--- public/utils/helpers.js
+++ public/utils/helpers.js
@@ -6,9 +6,11 @@ function switchTheme() {
     toggler.classList.remove('fa-star-o');
     body.classList.remove('barebones');
     toggler.classList.add('fa-star');
-    body.classList.add('colorful');
+    // body.classList.add('colorful');
+    body.classList.add('light');
   } else {
-    body.classList.remove('colorful');
+    // body.classList.remove('colorful');
+    body.classList.remove('light');
     toggler.classList.remove('fa-star');
     body.classList.add('barebones');
     toggler.classList.add('fa-star-o');
