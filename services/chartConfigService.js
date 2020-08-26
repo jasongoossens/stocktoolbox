8e0977a35b930fbd0a7a94c9fc80578b503baeef
Jason Goossens <jason.goossens@gmail.com>
2020-08-26 20:17:31 +0200

    refactored json objects to methods

--- services/chartConfigService.js
+++ services/chartConfigService.js
@@ -8,6 +8,7 @@ class ChartConfigService {
   keltnerUpper = [];
   keltnerMiddle = [];
   keltnerLower = [];
+  momentum = [];
   ttmSqueezeDots = [];
   ttmSqueezeHistogram = [];
 
@@ -32,6 +33,9 @@ class ChartConfigService {
   }
 
   sanitizeFinnHubDataForIndicators(chartData) {
+    console.log('lkjmlkj', chartData.v);
+    console.log('mlkjlmkj', chartData.momentum);
+
     chartData.t.forEach((element, index) => {
       this.dates.push(chartData.t[index] * 1000);
       this.prices.push([
@@ -45,6 +49,10 @@ class ChartConfigService {
 
     this.atr = chartData.atr.map((n) => Math.round(n * 100) / 100);
     this.ema = chartData.ema.map((n) => Math.round(n * 100) / 100);
+    this.momentum =
+      chartData.momentum !== undefined
+        ? chartData.momentum.map((n) => Math.round(n * 100) / 100)
+        : '';
     this.bBandUpper = chartData.upperband.map((n) => Math.round(n * 100) / 100);
     this.bBandMiddle = chartData.middleband.map(
       (n) => Math.round(n * 100) / 100
@@ -63,14 +71,16 @@ class ChartConfigService {
     });
 
     this.keltnerMiddle.forEach((element, index) => {
-      if (this.bBandUpper[index] > this.keltnerUpper[index]) {
-        //  Bbands greater than Keltner channels
-        this.ttmSqueezeDots.push(true);
+      if (
+        this.bBandUpper[index] > this.keltnerUpper[index] &&
+        this.bBandLower[index] < this.keltnerLower[index]
+      ) {
+        //  Bbands greater than Keltner channels == no squeeze
+        this.ttmSqueezeDots.push(null);
       } else {
-        this.ttmSqueezeDots.push(false);
+        this.ttmSqueezeDots.push(0);
       }
     });
-
     /*
 TTM Histogram calc
     https://www.tradingview.com/script/nqQ1DT5a-Squeeze-Momentum-Indicator-LazyBear/
@@ -113,6 +123,22 @@ linearReq(
     }
   }
 
+  getScaleX(dates, chartSize) {
+    return {
+      labels: dates.slice(chartSize),
+      step: 'day',
+      transform: {
+        type: 'date',
+        all: '%dd/%m',
+      },
+    };
+  }
+
+  getScaleValues(lowPrices, highPrices, chartSize) {
+    return `${Math.floor(Math.min(...lowPrices.slice(chartSize)) / 10) * 10}:
+    ${Math.ceil(Math.max(...highPrices.slice(chartSize)) / 10) * 10}`;
+  }
+
   createIndexConfig(name, resolution, chartSize = -253) {
     return JSON.stringify({
       type: 'stock',
@@ -134,12 +160,10 @@ linearReq(
         aspect: 'candlestick',
       },
       'scale-y': {
-        values: `${
-          Math.ceil((Math.max(...this.twelveDataHighPrices) * 1.02) / 10) * 10
-        }:
-          ${
-            Math.floor((Math.min(...this.twelveDataLowPrices) * 0.98) / 10) * 10
-          }`,
+        values: this.getScaleValues(
+          this.twelveDataLowPrices,
+          this.twelveDataHighPrices
+        ),
         label: {
           text: 'Prices',
         },
@@ -170,27 +194,15 @@ linearReq(
         adjustLayout: true,
       },
       plotarea: { margin: 'dynamic' },
-      'scale-x': {
-        labels: this.dates.slice(chartSize),
-        step: 'day',
-        transform: {
-          type: 'date',
-          all: '%dd/%m',
-        },
-      },
+      'scale-x': this.getScaleX(this.dates, chartSize),
       'scale-y': {
         'offset-start': '35%',
         format: '$%v',
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
         label: {
           text: 'Prices',
         },
@@ -251,175 +263,6 @@ linearReq(
     });
   }
 
-  createStockConfigBollingerBands(name, resolution, chartSize) {
-    return JSON.stringify({
-      type: 'mixed',
-      title: {
-        text: name + '(' + resolution + ')',
-        adjustLayout: true,
-      },
-      plotarea: { margin: 'dynamic' },
-      'scale-x': {
-        labels: this.dates.slice(chartSize),
-        step: 'day',
-        transform: {
-          type: 'date',
-          all: '%dd/%m',
-        },
-      },
-      'scale-y': {
-        'offset-start': '30%' /* distance from bottom */,
-        format: '$%v',
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
-        label: {
-          text: 'Prices',
-        },
-      },
-      'scale-y-2': {
-        'offset-end': '75%' /* distance from the top */,
-        placement: 'default',
-        blended: true,
-        label: {
-          text: 'Volume',
-        },
-      },
-      'scale-y-3': {
-        'offset-start': '30%' /* distance from bottom */,
-        placement: 'default',
-        blended: true,
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
-      },
-      'scale-y-4': {
-        'offset-start': '30%' /* distance from bottom */,
-        placement: 'default',
-        blended: true,
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
-      },
-      'scale-y-5': {
-        'offset-start': '30%' /* distance from bottom */,
-        placement: 'default',
-        blended: true,
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
-      },
-      'crosshair-x': {
-        'plot-label': {
-          text: 'Open: $%open<br>High: $%high<br>Low: $%low<br>Close: $%close',
-          decimals: 2,
-        },
-      },
-      series: [
-        {
-          type: 'stock',
-          scales: 'scale-x,scale-y',
-          'trend-up': {
-            'background-color': 'red',
-            'line-color': 'red',
-            'border-color': 'red',
-          },
-          'trend-down': {
-            'background-color': 'none',
-            'line-color': 'orange',
-            'border-color': 'orange',
-          },
-          'trend-equal': {
-            'background-color': 'red',
-            'line-color': 'red',
-            'border-color': 'red',
-          },
-          plot: {
-            aspect: 'candlestick',
-          },
-          values: this.prices.slice(chartSize),
-        },
-        {
-          type: 'bar',
-          scales: 'scale-x,scale-y-2',
-          'background-color': '#6382a5',
-          'guide-label': {
-            text: 'Volume: %v',
-            decimals: 0,
-          },
-          values: this.volumes.slice(chartSize),
-        },
-        {
-          type: 'line',
-          scales: 'scale-x,scale-y-3',
-          'background-color': '#6382a5',
-          'guide-label': {
-            text: '',
-          },
-          marker: {
-            'line-color': 'rbga(255,0,0,0.5',
-            size: 0,
-            'border-width': 0,
-          },
-          values: this.bBandUpper.slice(chartSize),
-        },
-        {
-          type: 'line',
-          scales: 'scale-x,scale-y-4',
-          'background-color': '#6382a5',
-
-          marker: {
-            size: 0,
-            'border-width': 0,
-          },
-          values: this.bBandMiddle.slice(chartSize),
-        },
-        {
-          type: 'line',
-          scales: 'scale-x,scale-y-5',
-          'background-color': '#6382a5',
-          'guide-label': {
-            text: '',
-            decimals: 2,
-          },
-          marker: {
-            size: 0,
-            'border-width': 0,
-          },
-          values: this.bBandLower.slice(chartSize),
-        },
-      ],
-    });
-  }
-
   createStockConfigBollingerBandsAtr(name, resolution, chartSize) {
     return JSON.stringify({
       type: 'mixed',
@@ -428,27 +271,15 @@ linearReq(
         adjustLayout: true,
       },
       plotarea: { margin: 'dynamic' },
-      'scale-x': {
-        labels: this.dates.slice(chartSize),
-        step: 'day',
-        transform: {
-          type: 'date',
-          all: '%dd/%m',
-        },
-      },
+      'scale-x': this.getScaleX(this.dates, chartSize),
       'scale-y': {
         'offset-start': '45%' /* distance from bottom */,
         format: '$%v',
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
         label: {
           text: 'Prices',
         },
@@ -474,46 +305,31 @@ linearReq(
         'offset-start': '45%' /* distance from bottom */,
         placement: 'default',
         blended: true,
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
       },
       'scale-y-5': {
         'offset-start': '45%' /* distance from bottom */,
         placement: 'default',
         blended: true,
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
       },
       'scale-y-6': {
         'offset-start': '45%' /* distance from bottom */,
         placement: 'default',
         blended: true,
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
       },
       'crosshair-x': {
         'plot-label': {
@@ -627,27 +443,15 @@ linearReq(
         adjustLayout: true,
       },
       plotarea: { margin: 'dynamic' },
-      'scale-x': {
-        labels: this.dates.slice(chartSize),
-        step: 'day',
-        transform: {
-          type: 'date',
-          all: '%dd/%m',
-        },
-      },
+      'scale-x': this.getScaleX(this.dates, chartSize),
       'scale-y': {
         'offset-start': '30%' /* distance from bottom */,
         format: '$%v',
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
         label: {
           text: 'Prices',
         },
@@ -664,46 +468,31 @@ linearReq(
         'offset-start': '30%' /* distance from bottom */,
         placement: 'default',
         blended: true,
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
       },
       'scale-y-4': {
         'offset-start': '30%' /* distance from bottom */,
         placement: 'default',
         blended: true,
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
       },
       'scale-y-5': {
         'offset-start': '30%' /* distance from bottom */,
         placement: 'default',
         blended: true,
-        values: `${
-          Math.floor(
-            (Math.min(...this.chartData.l.slice(chartSize)) * 0.98) / 10
-          ) * 10
-        }:
-          ${
-            Math.ceil(
-              (Math.max(...this.chartData.h.slice(chartSize)) * 1.02) / 10
-            ) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
       },
       'crosshair-x': {
         'plot-label': {
@@ -797,23 +586,15 @@ linearReq(
         adjustLayout: true,
       },
       plotarea: { margin: 'dynamic' },
-      'scale-x': {
-        labels: this.dates.slice(chartSize),
-        step: 'day',
-        transform: {
-          type: 'date',
-          all: '%dd/%m',
-        },
-      },
+      'scale-x': this.getScaleX(this.dates, chartSize),
       'scale-y': {
         'offset-start': '45%' /* distance from bottom */,
         format: '$%v',
-        values: `${
-          Math.floor(Math.min(...this.chartData.l.slice(chartSize)) / 10) * 10
-        }:
-          ${
-            Math.ceil(Math.max(...this.chartData.h.slice(chartSize)) / 10) * 10
-          }`,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
         label: {
           text: 'Prices',
         },
@@ -826,6 +607,7 @@ linearReq(
           text: 'Volume',
         },
       },
+      // squeeze dots
       'scale-y-3': {
         'offset-end': '55%' /* distance from the top */,
         'offset-start': '20%' /* distance from bottom */,
@@ -834,6 +616,57 @@ linearReq(
         label: {
           text: 'TTM_Squeeze',
         },
+      },
+      // squeeze momentum
+      'scale-y-4': {
+        'offset-end': '55%' /* distance from the top */,
+        'offset-start': '20%' /* distance from bottom */,
+        placement: 'default',
+        blended: true,
+      },
+      // bbol upper
+      'scale-y-5': {
+        'offset-start': '45%' /* distance from bottom */,
+        placement: 'default',
+        blended: true,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
+      },
+      // bbol lower
+      'scale-y-6': {
+        'offset-start': '45%' /* distance from bottom */,
+        placement: 'default',
+        blended: true,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
+      },
+      // keltner upper
+      'scale-y-7': {
+        'offset-start': '45%' /* distance from bottom */,
+        placement: 'default',
+        blended: true,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
+      },
+      // keltner lower
+      'scale-y-8': {
+        'offset-start': '45%' /* distance from bottom */,
+        placement: 'default',
+        blended: true,
+        values: this.getScaleValues(
+          this.chartData.l,
+          this.chartData.h,
+          chartSize
+        ),
       },
       'crosshair-x': {
         'plot-label': {
@@ -877,7 +710,69 @@ linearReq(
         },
         {
           type: 'line',
-          scales: 'scale-x,scale-y-3',
+          scales: 'scale-x,scale-y-3,scale-y-4',
+          'background-color': '#6382a5',
+          'guide-label': {
+            text: '',
+          },
+          marker: {
+            'background-color': '#FF0066',
+            size: 3,
+          },
+          values: this.ttmSqueezeDots.slice(chartSize),
+          'line-width': 0,
+        },
+        {
+          type: 'bar',
+          scales: 'scale-x,scale-y-3,scale-y-4',
+          'background-color': '#6382a5',
+          'guide-label': {
+            text: '',
+          },
+          marker: {
+            'background-color': '#FF0066',
+            size: 0,
+          },
+          values: this.momentum.slice(chartSize),
+          'line-color': '#FF0000',
+          'line-width': 3,
+        },
+        {
+          type: 'line',
+          scales: 'scale-x,scale-y-5',
+          'background-color': '#6382a5',
+          'guide-label': {
+            text: '',
+          },
+          marker: {
+            backgroundColor: '#FF0000',
+            size: 0,
+            'border-width': 0,
+          },
+          values: this.bBandUpper.slice(chartSize),
+          'line-color': '#FF0000',
+          'line-width': 1,
+        },
+        {
+          type: 'line',
+          scales: 'scale-x,scale-y-6',
+
+          'guide-label': {
+            text: '',
+            decimals: 2,
+          },
+          marker: {
+            backgroundColor: '#FF0000',
+            size: 0,
+            'border-width': 0,
+          },
+          values: this.bBandLower.slice(chartSize),
+          'line-color': '#FF0000',
+          'line-width': 1,
+        },
+        {
+          type: 'line',
+          scales: 'scale-x,scale-y-7',
           'background-color': '#6382a5',
           'guide-label': {
             text: '',
@@ -888,6 +783,20 @@ linearReq(
             'border-width': 0,
           },
           values: this.keltnerUpper.slice(chartSize),
+        },
+        {
+          type: 'line',
+          scales: 'scale-x,scale-y-8',
+          'background-color': '#6382a5',
+          'guide-label': {
+            text: '',
+          },
+          marker: {
+            'line-color': 'rbga(255,0,0,0.5',
+            size: 0,
+            'border-width': 0,
+          },
+          values: this.keltnerLower.slice(chartSize),
         },
       ],
     });
